a1a743d8586d0abe1a83b7c0abbe51f4b8b1dfac
SofiaAlvarez01 <89089343+SofiaAlvarez01@users.noreply.github.com>
2021-08-25 20:04:00 -0500

    Update exercise.py

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,8 +1,23 @@
 
 def main():
     #Escribe tu código debajo de esta línea
-    pass
+import math
+edad= int(input("Ingresa tu edad: "))
+if edad>=18:
+    identificacion= str(input("¿Tienes identificación oficial? (s/n): "))
 
+    if (identificacion== "s"):
+        print ("Trámite de licencia concedido")
+
+    elif (identificacion== "n"):
+        print ("No cumples requisitos")
+
+    elif (identificacion != "s" != "n"):
+        print ("Respuesta incorrecta")        
+else:
+    print("No cumples con los requisitos")    
+
+pass
 
 if __name__ == '__main__':
     main()
