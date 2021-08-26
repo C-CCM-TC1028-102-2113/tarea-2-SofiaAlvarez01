b297c2e81f4fa5ea77d2069ba17aab1b18df80dd
SofiaAlvarez01 <89089343+SofiaAlvarez01@users.noreply.github.com>
2021-08-25 21:03:54 -0500

    Update exercise.py

--- assignments/06BMI/src/exercise.py
+++ assignments/06BMI/src/exercise.py
@@ -1,5 +1,26 @@
 def main():
     #escribe tu código abajo de esta línea
+    import math
+peso = float(input("Peso en kg: "))
+altura = float(input("Altura en m: "))
+
+if peso>0 and altura>0:
+    índice= (peso)/(altura**2)
+    if índice<20:
+        print ("PESO BAJO")
+    elif 20 <= índice < 25:
+        print ("NORMAL")
+    elif 25 <= índice < 30:
+        print ("SOBREPESO")
+    elif 30 <= índice < 40:
+        print ("OBESIDAD")
+    elif índice >= 40:
+        print ("OBESIDAD MORBIDA")
+
+
+else:
+    print("Revisa tus datos, alguno de ellos es erróneo.")
+
     pass
 if __name__=='__main__':
     main()
